9e1ab31d0f2c41e2fc587078b57a7da6cc8e3b0a
ulyapebriyana <ulyapebriyanaalisaputra@gmail.com>
2024-03-30 22:36:01 +0700

    comment unnecessary

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,60 +1,70 @@
-"use client";
-
-import { Button, buttonVariants } from "@/components/ui/button";
-import Link from "next/link";
-import FormRegistration from "@/components/registration-member";
-import { useEffect } from "react";
-
-declare global {
-  interface Window {
-      snap: any
-  }
-}
+import React from 'react'
 
 const Dashboard = () => {
-  useEffect(() => {
-    const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
-    const myMidtransClientKey = process.env.CLIENT_KEY as string;
-
-    let scriptTag = document.createElement("script");
-    
-    scriptTag.src = midtransScriptUrl;
-    scriptTag.setAttribute("data-client-key", myMidtransClientKey);
-    scriptTag.async = true
-
-    document.body.appendChild(scriptTag);
-
-    return () => {
-      document.body.removeChild(scriptTag);
-    };
-  }, []);
-
-  const handlePay = async () => {
-    const getToken = await fetch(`http://localhost:3000/api/payment`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        order_id: "121212hfdf",
-        gross_amount: 20000,
-      }),
-    });
-
-    const data = await getToken.json();
-    console.log(data);
-
-    window.snap.pay(data.data.token);
-  };
-
   return (
-    <main className="h-screen w-full">
-      <div className="flex items-center justify-center h-full">
-        <FormRegistration />
-      </div>
-      <Button onClick={handlePay}>here</Button>
-    </main>
-  );
-};
+    <div>Dashboard</div>
+  )
+}
 
-export default Dashboard;
+export default Dashboard
+
+// "use client";
+
+// import { Button, buttonVariants } from "@/components/ui/button";
+// import Link from "next/link";
+// import FormRegistration from "@/components/registration-member";
+// import { useEffect } from "react";
+
+// declare global {
+//   interface Window {
+//       snap: any
+//   }
+// }
+
+// const Dashboard = () => {
+//   useEffect(() => {
+//     const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
+//     const myMidtransClientKey = process.env.CLIENT_KEY as string;
+
+//     let scriptTag = document.createElement("script");
+    
+//     scriptTag.src = midtransScriptUrl;
+//     scriptTag.setAttribute("data-client-key", myMidtransClientKey);
+//     scriptTag.async = true
+
+//     document.body.appendChild(scriptTag);
+
+//     return () => {
+//       document.body.removeChild(scriptTag);
+//     };
+//   }, []);
+
+//   const handlePay = async () => {
+//     const getToken = await fetch(`http://localhost:3000/api/payment`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         order_id: "121212hfdf",
+//         gross_amount: 20000,
+//       }),
+//     });
+
+//     const data = await getToken.json();
+//     console.log(data);
+
+//     window.snap.pay(data.data.token);
+//   };
+
+//   return (
+//     <main className="h-screen w-full">
+//       <div className="flex items-center justify-center h-full">
+//         <FormRegistration />
+//       </div>
+//       <Button onClick={handlePay}>here</Button>
+//     </main>
+//   );
+// };
+
+// export default Dashboard;
