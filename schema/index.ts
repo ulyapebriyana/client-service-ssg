a6432c1f9eba20b6e1dcd14cf8a81762ec34b3ff
ulyapebriyana <ulyapebriyanaalisaputra@gmail.com>
2024-04-14 19:50:02 +0700

    add schemas form

--- schema/index.ts
+++ schema/index.ts
@@ -1,11 +1,11 @@
 import { z } from "zod"
 
 export const registerSchema = z.object({
-    name: z.string().min(3).max(30),
+    name: z.string().min(3, "3 Minimal karakter").max(30, "30 Maksimal karakter"),
     email: z.string().email("Email tidak valid"),
-    telegramId: z.string().min(4).max(15),
-    password: z.string().min(6).max(10),
-    passwordConfirmation: z.string().min(6).max(10)
+    telegramId: z.string().min(4, "4 Minimal karakter").max(15, "15 Maksimal karakter"),
+    password: z.string().min(6, "6 Minimal karakter").max(10, "10 Maksimal karakter"),
+    passwordConfirmation: z.string().min(6, "6 Minimal karakter").max(10, "10 Maksimal karakter")
 }).refine((values) => {
     return values.password === values.passwordConfirmation
 }, {
@@ -23,8 +23,8 @@ export const forgotPasswordSchema = z.object({
 })
 
 export const resetPasswordSchema = z.object({
-    password: z.string().min(6).max(10),
-    passwordConfirmation: z.string().min(6).max(10)
+    password: z.string().min(6, "Minimal 6 karakter").max(10, "10 Maksimal karakter"),
+    passwordConfirmation: z.string().min(6, "Minimal 6 karakter").max(10, "10 Maksimal karakter")
 }).refine((values) => {
     return values.password === values.passwordConfirmation
 }, {
